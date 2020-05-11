4858628b6199060ad417299af9c0478cab5d8616
Srijan-18 <srijan.mishrasir@gmail.com>
2020-05-11 23:58:07 +0530

    Daily Wage calculation including part time scenario and its Display

--- employeeWage/EmployeeWageDay19.java
+++ employeeWage/EmployeeWageDay19.java
@@ -3,15 +3,20 @@ package com.employeeWage;
 class EmployeeWageDay19 {
 	public static void main(String args[])
 	{	
-		final int isPresent=1;
+		final int isPresent=1 ,isPartTime=2;
 		int empDailyWage=0, empDailyHours=0, empWagePerHour=20;
 		System.out.println("Welcome to Employee Wage Computation Program");
-		int empCheck=(int)(Math.floor(Math.random()*10)%2);
+		int empCheck=(int)(Math.floor(Math.random()*10)%3);
 		if(empCheck==isPresent)
 		{	
 			System.out.println("Employee is Present");
 			empDailyHours=8;
 		}	
+		else if(empCheck==isPartTime)
+		{
+			System.out.println("Employee is present part time");
+			empDailyHours=4;
+		}
 		else
 			System.out.println("Employee is Absent");
 		empDailyWage=empDailyHours*empWagePerHour;
